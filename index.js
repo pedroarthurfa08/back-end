d28d6a4f9860038c5c19a13ec73f392bf9caebf6
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-16 20:22:53 -0300

    comit

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ app.set('views', 'views')
 
 app.set('view engine', 'ejs')
 
-app.post('/ola', (req, res) => {
-    const nome = req.body.nome
-    res.send(`Olá ${nome}`)
-})
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/ola_form',(req, res) => {
+    res.render('ola_form')
+})
+
+app.post('/ola', (req, res) => {
+    const nome = req.body.nome
+    res.render('ola_resposta', {nome: nome})
 })
 
 app.listen(port, () => {
