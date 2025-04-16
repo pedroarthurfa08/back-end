8897e64adf1e683e9ca8774a31f61af97308154a
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-16 20:12:59 -0300

    comit

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extended: false}))
 
+app.set('views', 'views')
+
+app.set('view engine', 'ejs')
+
+app.post('/ola', (req, res) => {
+    const nome = req.body.nome
+    res.send(`Olá ${nome}`)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
