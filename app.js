2e574b17977feffd55f646ebb2425ea32a0d67d5
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-18 13:04:03 -0300

    fazendo a soma

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const port = 3000
 app.use(express.urlencoded({extended: false}))
 
 app.set('views', 'views')
-
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
@@ -19,6 +18,18 @@ app.get('/ola_form',(req, res) => {
 app.post('/ola', (req, res) => {
     const nome = req.body.nome
     res.render('ola_resposta', {nome: nome})
+})
+
+app.post('/soma_form', (req, res) =>{
+  res.render('soma_form')
+})
+
+app.post("/soma", (req, res) => {
+  const n1 = Number(req.body.numero1)
+  const n2 = Number(req.body.numero2)
+  const soma = n1 + n2
+
+  res.render('soma_resposta',{soma: soma})
 })
 
 app.listen(port, () => {
